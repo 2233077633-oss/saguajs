bd3da28d476369d55d6d4177b3a7adf7acdd276a
2233077633-oss <2233077633@correo.ler.uam.mx>
2026-06-12 13:53:36 -0600

    cuatro

--- sketch.js
+++ sketch.js
@@ -1,3 +1,13 @@
+let chamarra = 0;
+let playera = 0;
+let pantalon = 0;
+let ropainterior = 0;
+let zapatos = 0;
+
+
+
+
+
 function setup() 
 {
 	createCanvas(windowWidth, windowHeight);
@@ -6,11 +16,17 @@ function setup()
 
 function draw()
 {
-
 textSize(80);
 fill(0);
-textFont("Timmes New Roman");
-text("¿Cuántas prendas traes el día de hoy?" , 400, height/2);
+textFont("Timmes New Roman"); 
 
+text("s/agua", width/2, 100);
+text("¿Cuántas prendas traes el día de hoy?" ,400, height/2);
+text("chamarra", 400, height/2 + 100);
+
+// Desactivar texto sugerido y buscar en referencias de p5.js input text.
+// sustituir valores de variables.
+// condicionales. 
+text("_", 400, height/2 + 200);
 
 }
